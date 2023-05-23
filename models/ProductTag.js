b2a27338ac548194210f5fba4025f7a06b1c7850
Project Faust <pentecost.cody.lane@gmail.com>
2023-05-23 19:27:22 -0400

    added table columns to reference fk of Tag and Category

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -2,11 +2,33 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { };
 
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // references id of Product from model
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    // references id of Tag from model
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
