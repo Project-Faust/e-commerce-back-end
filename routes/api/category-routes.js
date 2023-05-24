2abab94dc48b74b8cea9237c387ecef7f0fef975
Project Faust <pentecost.cody.lane@gmail.com>
2023-05-24 19:45:50 -0400

    fixed delete issue

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -62,17 +62,18 @@ router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
     // find target category
-    const categoryDelete = await Category.findAll({
-      where: {
-        id: req.params.id
-      }
-    });
+    const categoryId = req.params.id;
+    const categoryDelete = await Category.findByPk(categoryId);
     // return 404 if category not found
     if (!categoryDelete) {
       return res.status(404).json({ error: 'Category not found' });
     }
     // destroy category
-    await Category.destroy();
+    await Category.destroy({
+      where: {
+        id: categoryId
+      }
+    });
     res.status(200).json({ message: 'Category deleted successfully.' })
   } catch (error) {
     res.status(500).json(error);
